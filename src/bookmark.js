6ec92efe19c2166c35bab4c030a303320e36667d
techno-tanoC <develop.tanoc@gmail.com>
2016-02-18 15:46:39 +0900

    navigate current tab if new tab

--- src/bookmark.js
+++ src/bookmark.js
@@ -3,13 +3,18 @@ import React, { Component, PropTypes } from 'react'
 export default class Bookmark extends Component {
   constructor(props) {
     super(props)
-    this.openTab = this.openTab.bind(this)
+    this.open = this.open.bind(this)
   }
 
-  openTab() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
-      let index = tab[0].index + 1
-      chrome.tabs.create({ url: this.props.url, index: index, active: true });
+  open() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      let tab = tabs[0]
+      if(tab.url == "chrome://newtab/") {
+        chrome.tabs.update({ url: this.props.url })
+      } else {
+        let index = tab.index + 1
+        chrome.tabs.create({ url: this.props.url, index: index, active: true });
+      }
     }.bind(this));
   }
 
@@ -17,7 +22,7 @@ export default class Bookmark extends Component {
     let { title, url } = this.props
     let favicon = "http://www.google.com/s2/favicons?domain=" + url.split('/')[2];
     return (
-      <div className="bookmark shadow" onClick={ this.openTab }>
+      <div className="bookmark shadow" onClick={ this.open }>
         <div className="image">
           <img className="favicon" src={ favicon } />
         </div>
